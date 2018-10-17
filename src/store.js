97a0b900bab3e51df2478137851600c022ec4d0a
Samuel <samuelsantosomail@gmail.com>
2018-10-17 10:32:11 +0700

    Implement Debugger

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, createAppropriateStore } from 'redux';
 
 import reducers from './reducers'; //Import the reducer
 
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__());
+
 // Connect our store to the reducers
-export default createStore(reducers, applyMiddleware(thunk));
+export default store;
