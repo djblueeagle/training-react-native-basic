5f05fe667e60786f5f35bf8933f037b10d7d01e1
Bintoro Adi Guna <saviour.of.darkness@gmail.com>
2018-10-29 14:31:03 +0700

    Fix strange bug on React Native Debugger error

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware, createAppropriateStore, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from "redux-saga";
 import reducers from './reducers'; //Import the reducer
 import rootSagas from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducers, compose(applyMiddleware(sagaMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(
+    reducers,
+    compose(applyMiddleware(sagaMiddleware)),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 sagaMiddleware.run(rootSagas);
 
