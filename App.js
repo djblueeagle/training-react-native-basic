7490c2f46fa2a5df71391fcc4e6172791eb6a611
Bintoro Adi Guna <saviour.of.darkness@gmail.com>
2018-10-29 14:31:11 +0700

    Used React Navigation

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { createStackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import store from './src/store'; //Import the store
-import News from './src/Containers/News/News' //Import the component file
+import News from './src/Containers/News/News'; //Import the component file
 
-export default class App extends Component {
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }
+  };
+
   render() {
     return (
         <Provider store={store}>
           <News />
         </Provider>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  }
+}, {
+  initialRouteName: 'Home'
+});
+
+export default class App extends Component {
+  render() {
+    return (
+        <RootStack />
     );
   }
 }
